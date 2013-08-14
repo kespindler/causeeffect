dafa1a735fb1047e5e6d4cac8d6b309a05cf738e
Kurt <kespindler@gmail.com>
2013-08-14 12:17:26 -0400

    create_entropymats seems to be working

--- create_entropymats.py
+++ create_entropymats.py
@@ -10,14 +10,15 @@ import collections
 
 def discretize(d):
     nbins = np.ceil(3.5*np.std(d) / len(d)**1.3);
-    edges = np.linspace(np.amin(X),np.amax(X),nbins+1);
+    edges = np.linspace(np.amin(d),np.amax(d),nbins+1);
     return np.digitize(d, edges);
 
 def singleH(d, dtype):
     if dtype == 'Numerical':
         d = discretize(d)
     freq = np.bincount(d.astype('int32')).astype('float64') / len(d)
-    hpos = np.dot(log(np.freq), freq)
+    freqnz = freq[np.nonzero(freq)]
+    hpos = np.dot(np.log(freqnz), freqnz)
     return -hpos
 
 def jointH(a, b, atype, btype):
@@ -28,16 +29,22 @@ def jointH(a, b, atype, btype):
     
     proba = np.bincount(a.astype('int32')).astype('float64') / len(a)
     probb = np.bincount(b.astype('int32')).astype('float64') / len(b)
-    jointp = np.outer(proba, probb)
+
+    proba_nz = proba[np.nonzero(proba)]
+    probb_nz = probb[np.nonzero(probb)]
+
+    jointp = np.outer(proba_nz, probb_nz)
     hpos = np.sum(np.log(jointp) * jointp)
     return -hpos
 
+print 'Reading in data...'
 info = data_io.read_train_info()
 train = data_io.read_train_pairs()
 n_samples = len(info.values)
 conditional_entropy = np.zeros((n_samples, 2))
 
 for i in range(n_samples):
+    print 'Processing %d...' % (i, )
     a, b = train.values[i]
     atype, btype = info.values[i]
     h_joint = jointH(a, b, atype, btype)
@@ -45,8 +52,7 @@ for i in range(n_samples):
     h_b = singleH(b, btype)
     h_a_cond_b = h_joint - h_a
     h_b_cond_a = h_joint - h_b
-
     conditional_entropy[i,:] = [h_a_cond_b, h_b_cond_a]
 
 savemat('matlab/entropy.mat'.format(i),
-        {'entropy': conditional_entropy})
+        {'entropy': conditional_entropy}, oned_as='column')
